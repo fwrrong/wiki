1299e83937782244bc08baa791d0da5d8f6164fb
fwrrong <fwrrong@umich.edu>
2023-10-11 23:31:05 -0400

    3-5 build mybatis generator, generate demo

--- doc/all.sql
+++ doc/all.sql
@@ -1,10 +1,16 @@
 DROP TABLE IF EXISTS `test`;
-
 CREATE TABLE `test`(
                        `id` BIGINT NOT NULL COMMENT 'id',
                        `name` VARCHAR(50) COMMENT 'name',
                        `password` VARCHAR(50) COMMENT 'password',
                        PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'test';
-
 insert into `test` (id, name, password) values (1, 'test', 'password');
+
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo`(
+                       `id` BIGINT NOT NULL COMMENT 'id',
+                       `name` VARCHAR(50) COMMENT 'name',
+                       PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'test';
+insert into `demo` (id, name) values (1, 'test');
---
+++ src/main/java/com/weijun/wiki/mapper/DemoMapper.java
@@ -0,0 +1,30 @@
+package com.weijun.wiki.mapper;
+
+import com.weijun.wiki.domain.Demo;
+import com.weijun.wiki.domain.DemoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
