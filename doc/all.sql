4ab6f21d3738d0bd7e711c03cb8568eded82a73e
fwrrong <fwrrong@umich.edu>
2023-10-12 01:30:02 -0400

    3-6 add ebook sql, generate mapper

--- doc/all.sql
+++ doc/all.sql
@@ -1,3 +1,42 @@
+# 电子书表
+drop table if exists `ebook`;
+create table `ebook` (
+                         `id` bigint not null comment 'id',
+                         `name` varchar(50) comment 'name',
+                         `category1_id` bigint comment 'cat1',
+                         `category2_id` bigint comment 'cat2',
+                         `description` varchar(200) comment 'description',
+                         `cover` varchar(200) comment 'cover',
+                         `doc_count` int not null default 0 comment 'doc_count',
+                         `view_count` int not null default 0 comment 'view_count',
+                         `vote_count` int not null default 0 comment 'vote_count',
+                         primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='eBook';
+
+insert into `ebook` (id, name, description) values (1, 'Spring Boot Intro', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (2, 'VueIntro', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (3, 'Python Intro', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (4, 'Mysql Intro', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (5, 'Oracle Intro', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
+
+drop table if exists `test`;
+create table `test` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment 'name',
+                        `password` varchar(50) comment 'password',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='test';
+
+insert into `test` (id, name, password) values (1, 'test', 'password');
+
+
+
+
+
+
+
+
+
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test`(
                        `id` BIGINT NOT NULL COMMENT 'id',
