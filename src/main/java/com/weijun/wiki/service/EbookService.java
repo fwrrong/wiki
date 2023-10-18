e13f82c7155c9ff8a77a957628b7dc1540662dc3
fwrrong <fwrrong@umich.edu>
2023-10-18 01:44:21 -0400

    3-8 build CopyUtil and encapsulates BeanUtils

--- src/main/java/com/weijun/wiki/service/EbookService.java
+++ src/main/java/com/weijun/wiki/service/EbookService.java
@@ -6,11 +6,10 @@ import com.weijun.wiki.domain.EbookExample.Criteria;
 import com.weijun.wiki.mapper.EbookMapper;
 import com.weijun.wiki.req.EbookReq;
 import com.weijun.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.weijun.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,12 +24,17 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebooksList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebooksList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+//            // 单个复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//
+//            respList.add(ebookResp);
+//        }
+        // list copy
+        List<EbookResp> respList = CopyUtil.copyList(ebooksList, EbookResp.class);
         return respList;
     }
 
