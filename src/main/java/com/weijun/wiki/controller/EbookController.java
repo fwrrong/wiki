2b31c1f41082189cf29cd8349b1dcd4c2b07fcc1
fwrrong <fwrrong@umich.edu>
2023-10-17 21:28:44 -0400

    3-6 add CommonResp, future api return value will be CommonResp Class

--- src/main/java/com/weijun/wiki/controller/EbookController.java
+++ src/main/java/com/weijun/wiki/controller/EbookController.java
@@ -2,6 +2,7 @@ package com.weijun.wiki.controller;
 
 import com.weijun.wiki.domain.Ebook;
 import com.weijun.wiki.service.EbookService;
+import com.weijun.wiki.resp.CommonResp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp =  new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
