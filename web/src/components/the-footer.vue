a22ba2e7253e9d2ece149cea09a86188af4fe16a
fwrrong <fwrrong@umich.edu>
2023-10-19 02:48:40 -0400

    4.7 build the-footer component

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    WiKi Created by Weijun Fan
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
